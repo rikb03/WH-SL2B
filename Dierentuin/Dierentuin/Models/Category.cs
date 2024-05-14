cf5ff6a148ec25d64fbe56beb47f41e8cc88893c
Rik Bakker <rikbakker03@gmail.com>
2024-05-14 20:03:44 +0200

    Added Categories

---
+++ Dierentuin/Dierentuin/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dierentuin.Models
+{
+    [Table("categories")]
+    public class Category
+    {
+        // Database things
+        [Key]
+        public int Id { get; set; } // Primary key id
+
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; } // Name of the category
+
+        [StringLength(255)]
+        [Required]
+        public string Description { get; set; } // Description of the category
+    }
+}
