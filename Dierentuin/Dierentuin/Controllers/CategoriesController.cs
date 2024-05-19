39548479342526bc1ba7f444ce3ef0c713c60856
Rik Bakker <rikbakker03@gmail.com>
2024-05-19 09:52:49 +0200

    Added count to category index

--- Dierentuin/Dierentuin/Controllers/CategoriesController.cs
+++ Dierentuin/Dierentuin/Controllers/CategoriesController.cs
@@ -22,6 +22,7 @@ namespace Dierentuin.Controllers
         // GET: Categories
         public async Task<IActionResult> Index()
         {
+            ViewBag.AnimalCount = await _context.Category.Include(c => c.Animals).CountAsync(); // Counts all the animals in this category
             return View(await _context.Category.ToListAsync());
         }
 
@@ -74,7 +75,11 @@ namespace Dierentuin.Controllers
                 return NotFound();
             }
 
-            var category = await _context.Category.FindAsync(id);
+            //var category = await _context.Category.FindAsync(id);
+            var category = await _context.Category
+                .Include(c => c.Animals) // Include related Animals
+                .FirstOrDefaultAsync(m => m.Id == id);
+
             if (category == null)
             {
                 return NotFound();
