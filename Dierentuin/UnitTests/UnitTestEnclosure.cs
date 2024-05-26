be3e2ab5a3fff435aee073116bb65e17ac9f2953
Rik Bakker <rikbakker03@gmail.com>
2024-05-26 19:52:46 +0200

    Added action sunrise for enclosure

---
+++ Dierentuin/UnitTests/UnitTestEnclosure.cs
@@ -0,0 +1,42 @@
+﻿using Dierentuin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class UnitTestEnclosure
+    {
+        [Fact]
+        public void Sunset_MixedActivityPatterns_ShouldReturnCorrectAction()
+        {
+            Enclosure enclosure = new Enclosure()
+            {
+                Animal = new List<Animal>
+                {
+                    new Animal { Name = "Kees", ActivityPattern = Animal.ActivityPatternType.Diurnal },
+                    new Animal { Name = "Piet", ActivityPattern = Animal.ActivityPatternType.Nocturnal },
+                    new Animal { Name = "Jan", ActivityPattern = Animal.ActivityPatternType.Cathermeral }
+                }
+            };
+
+            var result = enclosure.Sunset();
+
+            Assert.Equal("Kees is going to sleep", result["Kees"]);
+            Assert.Equal("Piet is waking up", result["Piet"]);
+            Assert.Equal("Jan is always active", result["Jan"]);
+        }
+
+        [Fact]
+        public void Sunset_EmptyEnclosure_ShouldReturnNull()
+        {
+            Enclosure enclosure = new Enclosure();
+
+            var result = enclosure.Sunset();
+
+            Assert.Null(result);
+        }
+    }
+}
