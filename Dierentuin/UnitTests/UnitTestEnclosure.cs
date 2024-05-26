ca4bd3e63b29fe681cd6795b3a926313ffd4b79b
Rik Bakker <rikbakker03@gmail.com>
2024-05-26 19:53:48 +0200

    Added action sunset for enclosure

--- Dierentuin/UnitTests/UnitTestEnclosure.cs
+++ Dierentuin/UnitTests/UnitTestEnclosure.cs
@@ -38,5 +38,35 @@ namespace UnitTests
 
             Assert.Null(result);
         }
+
+        [Fact]
+        public void Sunrise_MixedActivityPatterns_ShouldReturnCorrectAction()
+        {
+            Enclosure enclosure = new Enclosure()
+            {
+                Animal = new List<Animal>
+                {
+                    new Animal { Name = "Kees", ActivityPattern = Animal.ActivityPatternType.Diurnal },
+                    new Animal { Name = "Piet", ActivityPattern = Animal.ActivityPatternType.Nocturnal },
+                    new Animal { Name = "Jan", ActivityPattern = Animal.ActivityPatternType.Cathermeral }
+                }
+            };
+
+            var result = enclosure.Sunset();
+
+            Assert.Equal("Kees is waking up", result["Kees"]);
+            Assert.Equal("Piet is going to sleep", result["Piet"]);
+            Assert.Equal("Jan is always active", result["Jan"]);
+        }
+
+        [Fact]
+        public void Sunrise_EmptyEnclosure_ShouldReturnNull()
+        {
+            Enclosure enclosure = new Enclosure();
+
+            var result = enclosure.Sunset();
+
+            Assert.Null(result);
+        }
     }
 }
