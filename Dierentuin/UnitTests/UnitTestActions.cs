2722091ddf34d8c2c0c90fb40441606b092abde0
Rik Bakker <rikbakker03@gmail.com>
2024-05-26 14:24:55 +0200

    Added unit tests for sunset action for animals

---
+++ Dierentuin/UnitTests/UnitTestActions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dierentuin.Models;
+
+namespace UnitTests
+{
+    public class UnitTestActions
+    {
+        [Fact]
+        public void Sunset_DiurnalAnimal_ShouldGoingToSleep()
+        {
+            Animal animal = new Animal() { ActivityPattern = Animal.ActivityPatternType.Diurnal};
+
+            var result = animal.Sunset();
+
+            Assert.Equal("Going to sleep", result);
+        }
+
+        public void Sunset_NocturnalAnimal_ShouldWakingUp()
+        {
+            Animal animal = new Animal() { ActivityPattern = Animal.ActivityPatternType.Nocturnal };
+
+            var result = animal.Sunset();
+
+            Assert.Equal("Waking up", result);
+        }
+
+        public void Sunset_CathermeralAnimal_ShouldBeAlwaysActive()
+        {
+            Animal animal = new Animal() { ActivityPattern = Animal.ActivityPatternType.Cathermeral };
+
+            var result = animal.Sunset();
+
+            Assert.Equal("Always active", result);
+        }
+    }
+}
