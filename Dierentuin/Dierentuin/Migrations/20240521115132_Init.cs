3b69197243ebe14d789b7390984380b7f08ed0a5
JoelMagalhaes <144008528+JoelMagalhaes@users.noreply.github.com>
2024-06-14 12:56:11 +0200

    Revert "t"
    This reverts commit 693d21fe349b5794b7e80200e6aa6d1e0a380dfa.

--- Dierentuin/Dierentuin/Migrations/20240521115132_Init.cs
+++ Dierentuin/Dierentuin/Migrations/20240521115132_Init.cs
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<<< HEAD:Dierentuin/Dierentuin/Migrations/20240521115132_Init.cs
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
@@ -208,6 +209,111 @@ namespace Dierentuin.Migrations
     }
 }
 >>>>>>>> animal:Dierentuin/Dierentuin/Migrations/20240613210407_Init.cs
+=======
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dierentuin.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "enclosures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Climate = table.Column<int>(type: "int", nullable: false),
+                    Habitat = table.Column<int>(type: "int", nullable: false),
+                    SecurityLevel = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enclosures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    Dietary = table.Column<int>(type: "int", nullable: false),
+                    ActivityPattern = table.Column<int>(type: "int", nullable: false),
+                    Prey = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    spaceRequirement = table.Column<double>(type: "float", nullable: false),
+                    SecurityRequirement = table.Column<int>(type: "int", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    categories_id = table.Column<int>(type: "int", nullable: false),
+                    enclosures_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_animals_categories_categories_id",
+                        column: x => x.categories_id,
+                        principalTable: "categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_animals_enclosures_enclosures_id",
+                        column: x => x.enclosures_id,
+                        principalTable: "enclosures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_animals_categories_id",
+                table: "animals",
+                column: "categories_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_animals_enclosures_id",
+                table: "animals",
+                column: "enclosures_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "animals");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "enclosures");
+        }
+    }
+}
+>>>>>>> parent of f70c560 (Merge branch 'animal' into enclosure)
 =======
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
