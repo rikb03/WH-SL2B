ab5bb21efa903e8397f2a198317b682582692548
Rik Bakker <rikbakker03@gmail.com>
2024-06-30 14:07:42 +0200

    Added metrics to dierentuin project

--- Dierentuin/Dierentuin/Program.cs
+++ Dierentuin/Dierentuin/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Dierentuin.Data;
+using OpenTelemetry.Metrics;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +10,22 @@ builder.Services.AddControllersWithViews();
 // Add DbContext with SQLite
 builder.Services.AddDbContext<DierentuinContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DierentuinContext") ?? throw new InvalidOperationException("Connection string 'DierentuinContext' not found.")));
+
+// Add OpenTelemetry for scraping metrics with Prometheus
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
 
 var app = builder.Build();
 
@@ -26,6 +43,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapPrometheusScrapingEndpoint();
 
 app.MapControllerRoute(
     name: "default",
